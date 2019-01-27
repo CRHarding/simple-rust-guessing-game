96032112b50b808b019708239d2d1d1b00cfd4a9
Casey Harding <casey.r.harding@gmail.com>
2019-01-26 19:28:50 -0500

    [Main] Broke the error handling out into its own function to promote reuse.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 use std::{cmp::Ordering, io};
 use rand::Rng;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
+
 fn main() {
     println!("Guess the number!");
 
@@ -9,7 +30,7 @@ fn main() {
     loop {
         println!("Please input your guess.");
     
-        let mut guess = String::new();
+    	let mut guess = String::new();
         
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
@@ -17,13 +38,10 @@ fn main() {
         let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
-        };     
+        };
+
+	let _user_guess = Guess::new(guess);     
          
-        if guess < 1 || guess > 100 {
-            println!("The secret number will be between 1 and 100.");
-            continue;
-        }
- 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
